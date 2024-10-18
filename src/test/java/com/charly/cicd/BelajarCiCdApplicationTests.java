51f4b11120cf2426d45b600b9f97794632021862
Charly Micolas Butarbutar <charlymicolasbutarbutar@pinjamwinwin.com>
2024-10-18 14:20:56 +0700

    first commit

---
+++ src/test/java/com/charly/cicd/BelajarCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charly.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
